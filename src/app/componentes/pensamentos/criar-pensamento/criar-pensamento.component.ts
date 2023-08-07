85355824d4e772ae72c1ab5981766f8aeb6f947f
Filipe Silva <81836461+filipe-csilva@users.noreply.github.com>
2023-08-07 20:45:57 -0300

    ok

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,22 +10,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent {
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
 
-  constructor(private service: PensamentoService, private router: Router) {
+  formulario!: FormGroup;
+
+  constructor(private service: PensamentoService, private router: Router, private formBuilder: FormBuilder) {
 
   }
 
   ngOnInit(): void {
-
+    this.formulario = this.formBuilder.group({
+      conteudo : ['Formulário reativo'],
+      autoria : ['Angular'],
+      modelo : ['modelo1']
+    })
   }
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe()
+    this.service.criar(this.formulario.value).subscribe()
     {
       alert("Pensamento criado com sucesso");
       this.router.navigate(['/listarPensamento'])
